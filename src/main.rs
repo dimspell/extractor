3b289a2f8779a412aeab8b1cff02d43bea4db14f
dimspell <141545384+dimspell@users.noreply.github.com>
2026-02-08 21:30:25 +0100

    Use relative path, remove already handled code

--- src/main.rs
+++ src/main.rs
@@ -555,75 +555,77 @@ fn save_all() -> Result<(), Box<dyn std::error::Error>> {
 
     let conn = Connection::open("database.sqlite")?;
 
+    let main_path = Path::new("fixtures/Dispel");
+
     println!("Saving maps...");
-    let maps = all_map_ini::read_all_map_ini(&Path::new("fixtures/Dispel/AllMap.ini"))?;
+    let maps = all_map_ini::read_all_map_ini(&main_path.join("AllMap.ini"))?;
     save_maps(&conn, &maps)?;
     println!("Saving map_inis...");
-    let map_inis = map_ini::read_map_ini(&Path::new("fixtures/Dispel/Ref/Map.ini"))?;
+    let map_inis = map_ini::read_map_ini(&main_path.join("Ref/Map.ini"))?;
     save_map_inis(&conn, &map_inis)?;
     println!("Saving extras...");
-    let extras = extra_ini::read_extra_ini(&Path::new("fixtures/Dispel/Extra.ini"))?;
+    let extras = extra_ini::read_extra_ini(&main_path.join("Extra.ini"))?;
     save_extras(&conn, &extras)?;
     println!("Saving events...");
-    let events = event_ini::read_event_ini(&Path::new("fixtures/Dispel/Event.ini"))?;
+    let events = event_ini::read_event_ini(&main_path.join("Event.ini"))?;
     save_events(&conn, &events)?;
     println!("Saving monster_inis...");
-    let monster_inis = monster_ini::read_monster_ini(&Path::new("fixtures/Dispel/Monster.ini"))?;
+    let monster_inis = monster_ini::read_monster_ini(&main_path.join("Monster.ini"))?;
     save_monster_inis(&conn, &monster_inis)?;
     println!("Saving npc_inis...");
-    let npc_inis = npc_ini::read_npc_ini(&Path::new("fixtures/Dispel/Npc.ini"))?;
+    let npc_inis = npc_ini::read_npc_ini(&main_path.join("Npc.ini"))?;
     save_npc_inis(&conn, &npc_inis)?;
     println!("Saving wave_inis...");
-    let wave_inis = wave_ini::read_wave_ini(&Path::new("fixtures/Dispel/Wave.ini"))?;
+    let wave_inis = wave_ini::read_wave_ini(&main_path.join("Wave.ini"))?;
     save_wave_inis(&conn, &wave_inis)?;
     println!("Saving party_refs...");
-    let party_refs = party_ref::read_part_refs(&Path::new("fixtures/Dispel/Ref/PartyRef.ref"))?;
+    let party_refs = party_ref::read_part_refs(&main_path.join("Ref/PartyRef.ref"))?;
     save_party_refs(&conn, &party_refs)?;
     println!("Saving draw_items...");
-    let draw_items = draw_item::read_draw_items(&Path::new("fixtures/Dispel/Ref/DRAWITEM.ref"))?;
+    let draw_items = draw_item::read_draw_items(&main_path.join("Ref/DRAWITEM.ref"))?;
     save_draw_items(&conn, &draw_items)?;
     println!("Saving party_pgps...");
-    let party_pgps = party_pgp::read_party_pgps(&Path::new("fixtures/Dispel/NpcInGame/PartyPgp.pgp"))?;
+    let party_pgps = party_pgp::read_party_pgps(&main_path.join("NpcInGame/PartyPgp.pgp"))?;
     save_party_pgps(&conn, &party_pgps)?;
     println!("Saving dialogs...");
-    let dialogs = dialog::read_dialogs(&Path::new("fixtures/Dispel/NpcInGame/Dlgcat1.dlg"))?;
+    let dialogs = dialog::read_dialogs(&main_path.join("NpcInGame/Dlgcat1.dlg"))?; // fixtures/Dispel/NpcInGame/*.dlg
     save_dialogs(&conn, &dialogs)?;
 
     println!("Saving weapons...");
     let weapons =
-        weapons_db::read_weapons_db(&Path::new("fixtures/Dispel/CharacterInGame/weaponItem.db"))?;
+        weapons_db::read_weapons_db(&main_path.join("CharacterInGame/weaponItem.db"))?;
     save_weapons(&conn, &weapons)?;
     println!("Saving stores...");
-    let stores = store_db::read_store_db(&Path::new("fixtures/Dispel/CharacterInGame/STORE.DB"))?;
+    let stores = store_db::read_store_db(&main_path.join("CharacterInGame/STORE.DB"))?;
     save_stores(&conn, &stores)?;
     println!("Saving npcrefs...");
-    let npcrefs = npc_ref::read_npc_ref(&Path::new("fixtures/Dispel/NpcInGame/Npccat1.ref"))?;
+    let npcrefs = npc_ref::read_npc_ref(&main_path.join("NpcInGame/Npccat1.ref"))?; // fixtures/Dispel/NpcInGame/*.ref
     save_npc_refs(&conn, &npcrefs)?;
     println!("Saving monsters...");
-    let monsters = monster_db::read_monster_db(&Path::new("fixtures/Dispel/MonsterInGame/Monster.db"))?;
+    let monsters = monster_db::read_monster_db(&main_path.join("MonsterInGame/Monster.db"))?;
     save_monsters(&conn, &monsters)?;
     println!("Saving monster_refs...");
     let monster_refs =
-        monster_ref::read_monster_ref(&Path::new("fixtures/Dispel/MonsterInGame/Mondun01.ref"))?;
+        monster_ref::read_monster_ref(&main_path.join("MonsterInGame/Mondun01.ref"))?; // fixtures/Dispel/MonsterInGame/*.ref
     save_monster_refs(&conn, &monster_refs)?;
     println!("Saving misc_items...");
     let misc_items =
-        misc_item_db::read_misc_item_db(&Path::new("fixtures/Dispel/CharacterInGame/MiscItem.db"))?;
+        misc_item_db::read_misc_item_db(&main_path.join("CharacterInGame/MiscItem.db"))?;
     save_misc_items(&conn, &misc_items)?;
     println!("Saving heal_items...");
     let heal_items =
-        heal_item_db::read_heal_item_db(&Path::new("fixtures/Dispel/CharacterInGame/HealItem.db"))?;
+        heal_item_db::read_heal_item_db(&main_path.join("CharacterInGame/HealItem.db"))?;
     save_heal_items(&conn, &heal_items)?;
     println!("Saving extra_refs...");
-    let extra_refs = extra_ref::read_extra_ref(&Path::new("fixtures/Dispel/ExtraInGame/Extdun01.ref"))?;
+    let extra_refs = extra_ref::read_extra_ref(&main_path.join("ExtraInGame/Extdun01.ref"))?; // fixtures/Dispel/ExtraInGame/*.ref
     save_extra_refs(&conn, &extra_refs)?;
     println!("Saving event_items...");
     let event_items =
-        event_item_db::read_event_item_db(&Path::new("fixtures/Dispel/CharacterInGame/EventItem.db"))?;
+        event_item_db::read_event_item_db(&main_path.join("CharacterInGame/EventItem.db"))?;
     save_event_items(&conn, &event_items)?;
     println!("Saving edit_items...");
     let edit_items =
-        edit_item_db::read_edit_item_db(&Path::new("fixtures/Dispel/CharacterInGame/EditItem.db"))?;
+        edit_item_db::read_edit_item_db(&main_path.join("CharacterInGame/EditItem.db"))?;
     save_edit_items(&conn, &edit_items)?;
 
     // println!("Saving map_tiles...");
@@ -632,26 +634,6 @@ fn save_all() -> Result<(), Box<dyn std::error::Error>> {
     // save_map_tiles(&conn, &map_tiles)?;
     // save_maps(&conn, &maps).unwrap();
     // save_events(&conn, &events).unwrap();
-    // save_extras(&conn, &extras).unwrap();
-    // save_monster_inis(&conn, &monster_inis).unwrap();
-    // save_npc_inis(&conn, &npc_inis).unwrap();
-    // save_wave_inis(&conn, &wave_inis).unwrap();
-    // save_map_inis(&conn, &map_inis).unwrap();
-    // save_party_refs(&conn, &party_refs).unwrap();
-    // save_draw_items(&conn, &draw_items).unwrap();
-    // save_party_pgps(&conn, &party_pgps).unwrap();
-    // save_dialogs(&conn, &dialogs).unwrap();
-
-    // save_monsters(&conn, &monsters).unwrap();
-    // save_stores(&conn, &stores).unwrap();
-    // save_weapons(&conn, &weapons).unwrap();
-    // save_npc_refs(&conn, &npcrefs).unwrap();
-    // save_monster_refs(&conn, &monster_refs).unwrap();
-    // save_misc_items(&conn, &misc_items).unwrap();
-    // save_heal_items(&conn, &heal_items).unwrap();
-    // save_extra_refs(&conn, &extra_refs).unwrap();
-    // save_event_items(&conn, &event_items).unwrap();
-    // save_edit_items(&conn, &edit_items).unwrap();
 
     conn.close().unwrap();
 
