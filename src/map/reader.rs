f72d84bf231da7765c57cb9edf22f206087ee0db
dimspell <141545384+dimspell@users.noreply.github.com>
2026-03-30 00:24:21 +0200

    Revert "Fix rendering the cat2"
    This reverts commit 9d71f82e8508a4d56e499861e62dfd00d023bdf8.

--- src/map/reader.rs
+++ src/map/reader.rs
@@ -238,8 +238,7 @@ pub fn read_roof_tiles(
                 if some_flag > 0 {
                     println!("ReadRoofTiles TODO: {btl_tile_id:?} {some_flag}");
                 }
-                let real_btl_id = btl_tile_id >> 1;
-                btl_tiles.insert(coords, real_btl_id.into());
+                btl_tiles.insert(coords, btl_tile_id.into());
             }
         }
     }
