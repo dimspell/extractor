e869dc884881a9e0b01c80c33c10c2a23259ab26
dimspell <141545384+dimspell@users.noreply.github.com>
2026-07-01 22:42:18 +0200

    Add the FK between DialogueScript for the next dialogs

--- src/queries/create_table_dialogue_scripts.sql
+++ src/queries/create_table_dialogue_scripts.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS dialogue_scripts(
-    dialog_file TEXT,
-    id INTEGER,
+    dialog_file TEXT NOT NULL,
+    id INTEGER NOT NULL,
     required_event_id INTEGER,
     next_dialog_to_check INTEGER,
     dialog_type_id INTEGER,
@@ -9,5 +9,9 @@ CREATE TABLE IF NOT EXISTS dialogue_scripts(
     next_dialog_id1 INTEGER,
     next_dialog_id2 INTEGER,
     next_dialog_id3 INTEGER,
-    triggered_event_id INTEGER
+    triggered_event_id INTEGER,
+    PRIMARY KEY (dialog_file, id),
+    FOREIGN KEY (dialog_file, next_dialog_id1) REFERENCES dialogue_scripts(dialog_file, id),
+    FOREIGN KEY (dialog_file, next_dialog_id2) REFERENCES dialogue_scripts(dialog_file, id),
+    FOREIGN KEY (dialog_file, next_dialog_id3) REFERENCES dialogue_scripts(dialog_file, id)
 )
