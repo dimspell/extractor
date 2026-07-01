58dff67ed6da24f19781500627fd54e3173e7ecb
dimspell <141545384+dimspell@users.noreply.github.com>
2026-07-01 23:24:15 +0200

    Add the FK between the PartyRef and Npc.ini

--- src/queries/create_table_party_refs.sql
+++ src/queries/create_table_party_refs.sql
@@ -4,7 +4,7 @@ CREATE TABLE IF NOT EXISTS party_refs
     full_name             TEXT,
     job_name              TEXT,
     root_map_id           INTEGER,
-    npc_id                INTEGER,
+    npc_id                INTEGER NOT NULL REFERENCES npc_inis(id) ON DELETE CASCADE,
     dlg_when_not_in_party INTEGER,
     dlg_when_in_party     INTEGER,
     ghost_face_id         INTEGER
