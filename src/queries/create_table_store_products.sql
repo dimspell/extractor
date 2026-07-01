f606e655678158dc5f95eedcddcfe354a1f90a98
dimspell <141545384+dimspell@users.noreply.github.com>
2026-07-01 22:36:54 +0200

    Add the FK between Stores and StoreProducts

--- src/queries/create_table_store_products.sql
+++ src/queries/create_table_store_products.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS store_products
 (
-    store_id     INTEGER,
+    store_id     INTEGER REFERENCES stores(id) ON DELETE CASCADE,
     order_id     INTEGER,
     product_type INTEGER,
     product_id   INTEGER
